a2df6632ea27cbf616ddac1f662ef101ea3d8e96
Samuel Ogg <56794292+SamO135@users.noreply.github.com>
2023-04-20 13:03:21 +0100

    allow port 3000 for the client

--- softpack/app.py
+++ softpack/app.py
@@ -1,6 +1,7 @@
 from typing import Any
 import strawberry
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 from singleton_decorator import singleton
 from .query import Query
@@ -17,7 +18,18 @@ class Application:
         self.graphql_app = GraphQLRouter(self.schema)
         self.router = FastAPI()
         self.router.include_router(self.graphql_app, prefix="/graphql")
-
+        origins = [
+                "http://localhost",
+                "http://localhost:8080",
+                "http://localhost:3000",
+            ]
+        self.router.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
 
 
