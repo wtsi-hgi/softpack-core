ff8cfcb53c7c1e1ffa8a34730dce86427450553f
Samuel Ogg <56794292+SamO135@users.noreply.github.com>
2023-04-25 16:14:27 +0100

    updated schema added search queries for package collection, environments and users

--- softpack/query.py
+++ softpack/query.py
@@ -8,25 +8,48 @@ from .environments import Environments
 from .users import Users
 
 
-def env_count() -> int:
-    envs = Environments()
-    return len(envs.get())
-
 def all_environments() -> List[Environment]:
     envs = Environments()
     return envs.get()
 
-def all_packages() -> List[PackageCollection]:
-    pkgs = PackageCollections()
-    return pkgs.get()
+def all_package_collections() -> List[PackageCollection]:
+    collections = PackageCollections()
+    return collections.get()
 
 def all_users() -> List[User]:
     users = Users()
     return users.get()
 
+def find_environment(name: str) -> Environment:
+    envs = Environments().get()
+    matching_envs = [env for env in envs if env.name == name]
+    if len(matching_envs) > 0:
+        return matching_envs[0]
+    else:
+        return None
+    
+def find_package_collection(name: str) -> PackageCollection:
+    collections = PackageCollections().get()
+    matching_collections = [collection for collection in collections if collection.name == name]
+    if len(matching_collections) > 0:
+        return matching_collections[0]
+    else:
+        return None
+    
+def find_users(name: str) -> User:
+    users = Users().get()
+    matching_users = [user for user in users if user.name == name]
+    if len(matching_users) > 0:
+        return matching_users[0]
+    else:
+        return None
+
+
 @strawberry.type
 class Query:
-    env_count = strawberry.field(resolver=env_count)
     all_environments = strawberry.field(resolver=all_environments)
-    all_packages = strawberry.field(resolver=all_packages)
+    all_packages = strawberry.field(resolver=all_package_collections)
     all_users = strawberry.field(resolver=all_users)
+    find_environment = strawberry.field(resolver=find_environment)
+    find_package_collection = strawberry.field(resolver=find_package_collection)
+    find_users = strawberry.field(resolver=find_users)
