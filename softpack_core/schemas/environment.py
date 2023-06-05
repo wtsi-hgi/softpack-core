410c203fed3b54f50770bca7bc3425a0df847adf
Samuel Ogg <56794292+SamO135@users.noreply.github.com>
2023-06-05 16:58:31 +0100

    send build request to softpack-builder Error types and docstrings also added

--- softpack_core/schemas/environment.py
+++ softpack_core/schemas/environment.py
@@ -5,10 +5,11 @@ LICENSE file in the root directory of this source tree.
 """
 
 import os
-import uuid
 from dataclasses import dataclass
+from datetime import datetime
 from typing import Iterable, Optional
 
+import httpx
 import strawberry
 
 from softpack_core.artifacts import Artifacts
@@ -22,16 +23,18 @@ class Package(Spack.PackageBase):
 
     version: Optional[str] = None
 
+
 @strawberry.input
 class PackageInput(Package):
     """A Strawberry input model representing a pacakge."""
 
     def to_package(self):
         """Create a Package object from a PackageInput object.
-        
+
         Return: a Package object
         """
         return Package(**self.__dict__)
+
 
 @strawberry.type
 class Environment:
@@ -42,6 +45,8 @@ class Environment:
     path: str
     description: str
     packages: list[Package]
+    created: Optional[datetime]
+    state: Optional[str]
     artifacts = Artifacts()
 
     @classmethod
@@ -79,14 +84,25 @@ class Environment:
                 lambda package: Package(id=package, name=package),
                 spec.packages,
             ),  # type: ignore [call-arg]
+            created=None,
+            state=None,
         )
 
     @classmethod
-    def create(cls, name: str) -> "Environment":
+    def create(
+        cls,
+        name: str,
+        path: str,
+        description: Optional[str] = None,
+        packages: Optional[list[PackageInput]] = None,
+    ) -> "Environment":
         """Create an Environment object.
 
         Args:
             name: Name for an environment.
+            path: Path for an environment.
+            description: Description for an environment.
+            packages: List of packages in the environment.
 
         Returns:
             Environment: A newly created Environment.
@@ -94,46 +110,104 @@ class Environment:
         for env in Environment.iter():
             if name == env.name:
                 return EnvironmentAlreadyExistsError(**env.__dict__)
+        response = httpx.post(
+            "http://0.0.0.0:7080/environments/build",
+            json={
+                "name": name,
+                "model": {
+                    "description": description,
+                    "packages": [f"{pkg.name}" for pkg in packages],
+                },
+            },
+        ).json()
+        print(f"Create: {response}")
         return Environment(
-            id=uuid.uuid4().hex,
-            name=name,
-            path="users/username",
-            description="description",
-            packages=[Package(id="unknown", name="unknown-package")],
+            id=response['id'],
+            name=response['name'],
+            path=path,
+            description=description,
+            packages=map(lambda pkg: pkg.to_package(), packages),
+            created=datetime.strptime(
+                response['created'], '%Y-%m-%dT%H:%M:%S.%f%z'
+            ),
+            state=response['state']['type'],
         )  # type: ignore [call-arg]
 
     @classmethod
-    def update(cls, name: str, path: Optional[str]=None, description: Optional[str]=None, packages: Optional[list[PackageInput]]=None):
+    def update(
+        cls,
+        name: str,
+        path: Optional[str] = None,
+        description: Optional[str] = None,
+        packages: Optional[list[PackageInput]] = None,
+    ):
+        """Update an Environment object.
+
+        Args:
+            name: Name for an environment.
+            path: Path for an environment.
+            description: Description for an environment.
+            packages: List of packages in the environment.
+
+        Returns:
+            Environment: An updated Environment.
+        """
         for env in Environment.iter():
             if env.name == name:
+                response = httpx.post(
+                    "http://0.0.0.0:7080/environments/build",
+                    json={
+                        "name": name,
+                        "model": {
+                            "description": description,
+                            "packages": [pkg.name for pkg in packages],
+                        },
+                    },
+                ).json()
+                print(f"Update: {response}")
                 if path != None:
                     env.path = path
                 if description != None:
                     env.description = description
                 if packages != None:
                     env.packages = map(lambda pkg: pkg.to_package(), packages)
+                env.state = response['state']['type']
                 return env
-            return EnvironmentNotFoundError(name=name)
-    
+        return EnvironmentNotFoundError(name=name)
+
     @classmethod
     def delete(cls, name: str):
+        """Delete an Environment object.
+
+        Returns:
+            A string confirming the deletion of the Environment
+        """
         for env in Environment.iter():
             if env.name == name:
                 return f"Deleted {name}"
         return "An environment with that name was not found"
-    
-#Error types
+
+
+# Error types
 @strawberry.type
 class EnvironmentNotFoundError:
     """Environment not found"""
+
     name: str
+
 
 @strawberry.type
 class EnvironmentAlreadyExistsError(Environment):
     """Environment name already exists"""
 
-UpdateEnvironmentResponse = strawberry.union("UpdateEnvironmentResponse", [Environment, EnvironmentNotFoundError])
-CreateEnvironmentResponse = strawberry.union("CreateEnvironmentResponse", [Environment, EnvironmentAlreadyExistsError])
+
+UpdateEnvironmentResponse = strawberry.union(
+    "UpdateEnvironmentResponse", [Environment, EnvironmentNotFoundError]
+)
+CreateEnvironmentResponse = strawberry.union(
+    "CreateEnvironmentResponse", [Environment, EnvironmentAlreadyExistsError]
+)
+
 
 class EnvironmentSchema(BaseSchema):
     """Environment schema."""
@@ -150,4 +224,4 @@ class EnvironmentSchema(BaseSchema):
 
         createEnvironment: CreateEnvironmentResponse = Environment.create  # type: ignore
         updateEnvironment: UpdateEnvironmentResponse = Environment.update  # type: ignore
-        deleteEnvironment: str = Environment.delete # type: ignore
+        deleteEnvironment: str = Environment.delete  # type: ignore
