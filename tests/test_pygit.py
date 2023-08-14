c268d53b26fbefbbbf5bf0a158057418ddfd35dd
Samuel Ogg <56794292+SamO135@users.noreply.github.com>
2023-08-14 14:34:28 +0100

    pytest for pushing to repo

--- tests/test_pygit.py
+++ tests/test_pygit.py
@@ -4,20 +4,21 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+import pytest
 import pytest_mock
 import pygit2
 from pygit2 import Signature
 import tempfile
 from softpack_core.artifacts import Artifacts
 
-
-def test_commit():
+@pytest.fixture
+def new_repo():
     temp_dir = tempfile.TemporaryDirectory(ignore_cleanup_errors=True)
     path = temp_dir.name
     print(path)
     repo = pygit2.init_repository(path)
-    open(f"{path}/initial_file.txt", "w").close()
 
+    open(f"{path}/initial_file.txt", "w").close()
     index = repo.index
     index.add_all()
     index.write()
@@ -29,6 +30,13 @@ def test_commit():
     parents = []
     old_commit_oid = repo.create_commit(ref, author, committer, message, tree, parents)
 
+    return (repo, temp_dir, old_commit_oid)
+
+
+def test_commit(new_repo):
+    repo = new_repo[0]
+    temp_dir = new_repo[1]
+    old_commit_oid = new_repo[2]
     file_oid = repo.create_blob("test")
     tree = repo.head.peel(pygit2.Tree)
     tree_builder = repo.TreeBuilder(tree)
@@ -42,3 +50,11 @@ def test_commit():
 
     assert old_commit_oid != new_commit_oid
     assert new_commit_oid == repo_head
+
+def test_push(mocker):
+    artifacts = Artifacts()
+
+    push_mock = mocker.patch('pygit2.Remote.push')
+
+    artifacts.push(artifacts.repo)
+    push_mock.assert_called_once()
