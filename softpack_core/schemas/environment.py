d37febea2000da96a7c60a2941b446449e25708e
Samuel Ogg <56794292+SamO135@users.noreply.github.com>
2023-08-15 12:37:21 +0100

    reformatted with tox fixed a couple errors tox found also

--- softpack_core/schemas/environment.py
+++ softpack_core/schemas/environment.py
@@ -18,256 +18,6 @@ from softpack_core.schemas.base import BaseSchema
 from softpack_core.spack import Spack
 
 
-@strawberry.type
-class Package(Spack.PackageBase):
-    """A Strawberry model representing a package."""
-
-    version: Optional[str] = None
-
-
-@strawberry.input
-class PackageInput(Package):
-    """A Strawberry input model representing a package."""
-
-    id: Optional[str] = None
-
-    def to_package(self) -> Package:
-        """Create a Package object from a PackageInput object.
-
-        Return: a Package object
-        """
-        return Package(**vars(self))
-
-
-@strawberry.input
-class EnvironmentInput:
-    """A Strawberry input model representing an environment."""
-
-    name: str
-    path: str
-    description: str
-    packages: list[PackageInput]
-
-
-@strawberry.type
-class Environment:
-    """A Strawberry model representing a single environment."""
-
-    id: str
-    name: str
-    path: str
-    description: str
-    packages: list[Package]
-    state: Optional[str]
-    artifacts = Artifacts()
-
-    @classmethod
-    def iter(cls, all: bool = False) -> Iterable["Environment"]:
-        """Get an iterator over Environment objects.
-
-        Returns:
-            Iterable[Environment]: An iterator of Environment objects.
-        """
-        user = None
-        if not user:
-            # TODO: set username from the environment for now
-            # eventually this needs to be the name of the authenticated user
-            user = os.environ["USER"]
-        environment_folders = cls.artifacts.iter(user=user)
-        environment_objects = map(cls.from_artifact, environment_folders)
-        return filter(lambda x: x is not None, environment_objects)
-
-    @classmethod
-    def from_artifact(cls, obj: Artifacts.Object) -> "Environment":
-        """Create an Environment object from an artifact.
-
-        Args:
-            obj: An artifact object.
-
-        Returns:
-            Environment: An Environment object.
-        """
-        try:
-            spec = obj.spec()
-            return Environment(
-                id=obj.oid,
-                name=obj.name,
-                path=obj.path.parent,
-                description=spec.description,
-                packages=map(
-                    lambda package: Package(id=package, name=package),
-                    spec.packages,
-                ),  # type: ignore [call-arg]
-                state=None,
-            )
-        except KeyError:
-            return None
-
-    @classmethod
-    def create(cls, env: EnvironmentInput):
-        """Create an Environment.
-
-        Args:
-            env: Details of the new environment
-
-        Returns:
-            A message confirming the success or failure of the operation.
-        """
-        # Check if any field has been left empty
-        if any(len(value) == 0 for value in vars(env).values()):
-            return InvalidInputError(message="all fields must be filled in")
-
-        # Check if a valid path has been provided
-        user = os.environ["USER"]
-        if env.path not in ["groups/hgi", f"users/{user}"]:
-            return InvalidInputError(message="Invalid path")
-
-        # Check if an env with same name already exists at given path
-        if cls.artifacts.get(Path(env.path), env.name):
-            return EnvironmentAlreadyExistsError(
-                message="This name is already used in this location",
-                path=env.path,
-                name=env.name,
-            )
-
-        # Create folder with readme
-        new_folder_path = Path(env.path, env.name)
-        file_name = "README.md"
-        try:
-            tree_oid = cls.artifacts.create_file(
-                new_folder_path, file_name, "lorem ipsum", True
-            )
-            cls.artifacts.commit(
-                cls.artifacts.repo, tree_oid, "create environment folder"
-            )
-            cls.artifacts.push(cls.artifacts.repo)
-        except RuntimeError as e:
-            return InvalidInputError(message=str(e))
-
-        # Send build request
-        httpx.post(
-            "http://0.0.0.0:7080/environments/build",
-            json={
-                "name": f"{env.path}/{env.name}",
-                "model": {
-                    "description": env.description,
-                    "packages": [f"{pkg.name}" for pkg in env.packages],
-                },
-            },
-        )
-
-        return CreateEnvironmentSuccess(
-            message="Successfully scheduled environment creation"
-        )
-
-    @classmethod
-    def update(
-        cls,
-        env: EnvironmentInput,
-        current_path: str,
-        current_name: str,
-    ):
-        """Update an Environment.
-
-        Args:
-            env: Details of the updated environment
-            path: The path of the current environment
-            name: The name of the current environment
-
-        Returns:
-            A message confirming the success or failure of the operation.
-        """
-        # Check if any field has been left empty
-        if (
-            any(len(value) == 0 for value in vars(env).values())
-            or current_name == ""
-            or current_path == ""
-        ):
-            return InvalidInputError(message="all fields must be filled in")
-
-        # Check name and path have not been changed.
-        if env.path != current_path or env.name != current_name:
-            return InvalidInputError(message="cannot change name or path")
-
-        # Check if an environment exists at the specified path and name
-        if cls.artifacts.get(Path(current_path), current_name):
-            httpx.post(
-                "http://0.0.0.0:7080/environments/build",
-                json={
-                    "name": f"{env.path}/{env.name}",
-                    "model": {
-                        "description": env.description,
-                        "packages": [pkg.name for pkg in env.packages or []],
-                    },
-                },
-            )
-
-            return UpdateEnvironmentSuccess(
-                message="Successfully updated environment"
-            )
-
-        return EnvironmentNotFoundError(
-            message="No environment with this name found in this location.",
-            path=current_path,
-            name=current_name,
-        )
-
-    @classmethod
-    def delete(cls, name: str, path: str):
-        """Delete an Environment.
-
-        Args:
-            name: the name of of environment
-            path: the path of the environment
-
-        Returns:
-            A message confirming the success or failure of the operation.
-        """
-        if cls.artifacts.get(Path(path), name):
-            tree_oid = cls.artifacts.delete_environment(name, path)
-            cls.artifacts.commit(
-                cls.artifacts.repo, tree_oid, "delete environment"
-            )
-            cls.artifacts.push(cls.artifacts.repo)
-            return DeleteEnvironmentSuccess(
-                message="Successfully deleted the environment"
-            )
-
-        return EnvironmentNotFoundError(
-            message="No environment with this name found in this location.",
-            path=path,
-            name=name,
-        )
-
-    @classmethod
-    async def write_artifact(
-        cls, file: Upload, folder_path: str, file_name: str
-    ):
-        """Add a file to the Artifacts repo.
-
-        Args:
-            file: the file to add to the repo
-            folder_path: the path to the folder that the file will be added to
-            file_name: the name of the file
-        """
-        try:
-            contents = (await file.read()).decode()
-            tree_oid = cls.artifacts.create_file(
-                Path(folder_path), file_name, contents, overwrite=True
-            )
-            commit_oid = cls.artifacts.commit(
-                cls.artifacts.repo, tree_oid, "write artifact"
-            )
-            cls.artifacts.push(cls.artifacts.repo)
-            return WriteArtifactSuccess(
-                message="Successfully written artifact",
-                commit_oid=str(commit_oid),
-            )
-
-        except Exception as e:
-            return InvalidInputError(message=str(e))
-
-
 # Interfaces
 @strawberry.interface
 class Success:
@@ -374,6 +124,256 @@ WriteArtifactResponse = strawberry.union(
         InvalidInputError,
     ],
 )
+
+
+@strawberry.type
+class Package(Spack.PackageBase):
+    """A Strawberry model representing a package."""
+
+    version: Optional[str] = None
+
+
+@strawberry.input
+class PackageInput(Package):
+    """A Strawberry input model representing a package."""
+
+    id: Optional[str] = None
+
+    def to_package(self) -> Package:
+        """Create a Package object from a PackageInput object.
+
+        Return: a Package object
+        """
+        return Package(**vars(self))
+
+
+@strawberry.input
+class EnvironmentInput:
+    """A Strawberry input model representing an environment."""
+
+    name: str
+    path: str
+    description: str
+    packages: list[PackageInput]
+
+
+@strawberry.type
+class Environment:
+    """A Strawberry model representing a single environment."""
+
+    id: str
+    name: str
+    path: str
+    description: str
+    packages: list[Package]
+    state: Optional[str]
+    artifacts = Artifacts()
+
+    @classmethod
+    def iter(cls, all: bool = False) -> Iterable["Environment"]:
+        """Get an iterator over Environment objects.
+
+        Returns:
+            Iterable[Environment]: An iterator of Environment objects.
+        """
+        user = None
+        if not user:
+            # TODO: set username from the environment for now
+            # eventually this needs to be the name of the authenticated user
+            user = os.environ["USER"]
+        environment_folders = cls.artifacts.iter(user=user)
+        environment_objects = map(cls.from_artifact, environment_folders)
+        return filter(lambda x: x is not None, environment_objects)
+
+    @classmethod
+    def from_artifact(cls, obj: Artifacts.Object) -> Optional["Environment"]:
+        """Create an Environment object from an artifact.
+
+        Args:
+            obj: An artifact object.
+
+        Returns:
+            Environment: An Environment object.
+        """
+        try:
+            spec = obj.spec()
+            return Environment(
+                id=obj.oid,
+                name=obj.name,
+                path=obj.path.parent,
+                description=spec.description,
+                packages=map(
+                    lambda package: Package(id=package, name=package),
+                    spec.packages,
+                ),  # type: ignore [call-arg]
+                state=None,
+            )
+        except KeyError:
+            return None
+
+    @classmethod
+    def create(cls, env: EnvironmentInput) -> CreateResponse:
+        """Create an Environment.
+
+        Args:
+            env: Details of the new environment
+
+        Returns:
+            A message confirming the success or failure of the operation.
+        """
+        # Check if any field has been left empty
+        if any(len(value) == 0 for value in vars(env).values()):
+            return InvalidInputError(message="all fields must be filled in")
+
+        # Check if a valid path has been provided
+        user = os.environ["USER"]
+        if env.path not in ["groups/hgi", f"users/{user}"]:
+            return InvalidInputError(message="Invalid path")
+
+        # Check if an env with same name already exists at given path
+        if cls.artifacts.get(Path(env.path), env.name):
+            return EnvironmentAlreadyExistsError(
+                message="This name is already used in this location",
+                path=env.path,
+                name=env.name,
+            )
+
+        # Create folder with readme
+        new_folder_path = Path(env.path, env.name)
+        file_name = "README.md"
+        try:
+            tree_oid = cls.artifacts.create_file(
+                new_folder_path, file_name, "lorem ipsum", True
+            )
+            cls.artifacts.commit(
+                cls.artifacts.repo, tree_oid, "create environment folder"
+            )
+            cls.artifacts.push(cls.artifacts.repo)
+        except RuntimeError as e:
+            return InvalidInputError(message=str(e))
+
+        # Send build request
+        httpx.post(
+            "http://0.0.0.0:7080/environments/build",
+            json={
+                "name": f"{env.path}/{env.name}",
+                "model": {
+                    "description": env.description,
+                    "packages": [f"{pkg.name}" for pkg in env.packages],
+                },
+            },
+        )
+
+        return CreateEnvironmentSuccess(
+            message="Successfully scheduled environment creation"
+        )
+
+    @classmethod
+    def update(
+        cls,
+        env: EnvironmentInput,
+        current_path: str,
+        current_name: str,
+    ) -> UpdateResponse:
+        """Update an Environment.
+
+        Args:
+            env: Details of the updated environment
+            path: The path of the current environment
+            name: The name of the current environment
+
+        Returns:
+            A message confirming the success or failure of the operation.
+        """
+        # Check if any field has been left empty
+        if (
+            any(len(value) == 0 for value in vars(env).values())
+            or current_name == ""
+            or current_path == ""
+        ):
+            return InvalidInputError(message="all fields must be filled in")
+
+        # Check name and path have not been changed.
+        if env.path != current_path or env.name != current_name:
+            return InvalidInputError(message="cannot change name or path")
+
+        # Check if an environment exists at the specified path and name
+        if cls.artifacts.get(Path(current_path), current_name):
+            httpx.post(
+                "http://0.0.0.0:7080/environments/build",
+                json={
+                    "name": f"{env.path}/{env.name}",
+                    "model": {
+                        "description": env.description,
+                        "packages": [pkg.name for pkg in env.packages or []],
+                    },
+                },
+            )
+
+            return UpdateEnvironmentSuccess(
+                message="Successfully updated environment"
+            )
+
+        return EnvironmentNotFoundError(
+            message="No environment with this name found in this location.",
+            path=current_path,
+            name=current_name,
+        )
+
+    @classmethod
+    def delete(cls, name: str, path: str) -> DeleteResponse:
+        """Delete an Environment.
+
+        Args:
+            name: the name of of environment
+            path: the path of the environment
+
+        Returns:
+            A message confirming the success or failure of the operation.
+        """
+        if cls.artifacts.get(Path(path), name):
+            tree_oid = cls.artifacts.delete_environment(name, path)
+            cls.artifacts.commit(
+                cls.artifacts.repo, tree_oid, "delete environment"
+            )
+            cls.artifacts.push(cls.artifacts.repo)
+            return DeleteEnvironmentSuccess(
+                message="Successfully deleted the environment"
+            )
+
+        return EnvironmentNotFoundError(
+            message="No environment with this name found in this location.",
+            path=path,
+            name=name,
+        )
+
+    @classmethod
+    async def write_artifact(
+        cls, file: Upload, folder_path: str, file_name: str
+    ) -> WriteArtifactResponse:
+        """Add a file to the Artifacts repo.
+
+        Args:
+            file: the file to add to the repo
+            folder_path: the path to the folder that the file will be added to
+            file_name: the name of the file
+        """
+        try:
+            contents = (await file.read()).decode()
+            tree_oid = cls.artifacts.create_file(
+                Path(folder_path), file_name, contents, overwrite=True
+            )
+            commit_oid = cls.artifacts.commit(
+                cls.artifacts.repo, tree_oid, "write artifact"
+            )
+            cls.artifacts.push(cls.artifacts.repo)
+            return WriteArtifactSuccess(
+                message="Successfully written artifact",
+                commit_oid=str(commit_oid),
+            )
+
+        except Exception as e:
+            return InvalidInputError(message=str(e))
 
 
 class EnvironmentSchema(BaseSchema):
--- tests/test_pygit.py
+++ tests/test_pygit.py
@@ -5,18 +5,20 @@ LICENSE file in the root directory of this source tree.
 """
 
 import os
-import pytest
 import shutil
-import pytest_mock
-from pathlib import Path
-import pygit2
-from pygit2 import Signature
 import tempfile
+from pathlib import Path
+
+import pygit2
+import pytest
+from pygit2 import Signature
+
 from softpack_core.artifacts import Artifacts
+
 
 @pytest.fixture
 def new_repo():
-    temp_dir = tempfile.TemporaryDirectory(ignore_cleanup_errors=True)
+    temp_dir = tempfile.TemporaryDirectory()
     path = temp_dir.name
     repo = pygit2.init_repository(path)
 
@@ -30,28 +32,30 @@ def new_repo():
     message = "Initial commit"
     tree = index.write_tree()
     parents = []
-    old_commit_oid = repo.create_commit(ref, author, committer, message, tree, parents)
+    old_commit_oid = repo.create_commit(
+        ref, author, committer, message, tree, parents
+    )
 
     return (repo, temp_dir, old_commit_oid)
 
 
 def test_commit(new_repo):
     repo = new_repo[0]
-    temp_dir = new_repo[1]
     old_commit_oid = new_repo[2]
-    file_oid = repo.create_blob("test")
-    tree = repo.head.peel(pygit2.Tree)
-    tree_builder = repo.TreeBuilder(tree)
-    tree_builder.insert("new_file.txt", file_oid, pygit2.GIT_FILEMODE_BLOB)
-    new_tree = tree_builder.write()
-    
-    artifacts = Artifacts()
-    new_commit_oid = artifacts.commit(repo, new_tree, "commit new file")
-    repo_head = repo.head.peel(pygit2.Commit).oid
-    temp_dir.cleanup()
+    with new_repo[1]:
+        file_oid = repo.create_blob("test")
+        tree = repo.head.peel(pygit2.Tree)
+        tree_builder = repo.TreeBuilder(tree)
+        tree_builder.insert("new_file.txt", file_oid, pygit2.GIT_FILEMODE_BLOB)
+        new_tree = tree_builder.write()
 
-    assert old_commit_oid != new_commit_oid
-    assert new_commit_oid == repo_head
+        artifacts = Artifacts()
+        new_commit_oid = artifacts.commit(repo, new_tree, "commit new file")
+        repo_head = repo.head.peel(pygit2.Commit).oid
+
+        assert old_commit_oid != new_commit_oid
+        assert new_commit_oid == repo_head
+
 
 def test_push(mocker):
     artifacts = Artifacts()
@@ -61,73 +65,99 @@ def test_push(mocker):
     artifacts.push(artifacts.repo)
     push_mock.assert_called_once()
 
+
 def get_user_envs_tree(artifacts, oid) -> pygit2.Tree:
     new_tree = artifacts.repo.get(oid)
     return new_tree[artifacts.user_folder(os.environ["USER"])]
 
+
 def copy_of_repo(artifacts):
-    temp_dir = tempfile.TemporaryDirectory(ignore_cleanup_errors=True)
+    temp_dir = tempfile.TemporaryDirectory()
     shutil.copytree(artifacts.repo.path, temp_dir.name, dirs_exist_ok=True)
     artifacts.repo = pygit2.Repository(temp_dir.name)
     return temp_dir
 
+
 def get_user_path_without_environments(artifacts) -> Path:
     return Path(*(artifacts.user_folder(os.environ["USER"]).parts[1:]))
+
 
 def test_create_file():
     artifacts = Artifacts()
     with copy_of_repo(artifacts):
         new_test_env = "test_create_file_env"
-        assert new_test_env not in [obj.name for obj in artifacts.iter_user(os.environ["USER"])]
+        assert new_test_env not in [
+            obj.name for obj in artifacts.iter_user(os.environ["USER"])
+        ]
 
-        folder_path = Path(get_user_path_without_environments(artifacts), new_test_env)
-        oid = artifacts.create_file(str(folder_path), "file.txt", "lorem ipsum", True, False)
+        fname = "file.txt"
+
+        folder_path = Path(
+            get_user_path_without_environments(artifacts), new_test_env
+        )
+        oid = artifacts.create_file(
+            str(folder_path), fname, "lorem ipsum", True, False
+        )
 
         user_envs_tree = get_user_envs_tree(artifacts, oid)
         assert new_test_env in [obj.name for obj in user_envs_tree]
-        assert "file.txt" in [obj.name for obj in user_envs_tree[new_test_env]]
+        assert fname in [obj.name for obj in user_envs_tree[new_test_env]]
 
         artifacts.commit(artifacts.repo, oid, "commit file")
 
-        with pytest.raises(RuntimeError) as exc_info:   
-            artifacts.create_file(str(folder_path), "second_file.txt", "lorem ipsum", True, False)
+        with pytest.raises(RuntimeError) as exc_info:
+            artifacts.create_file(
+                str(folder_path), "second_file.txt", "lorem ipsum", True, False
+            )
         assert exc_info.value.args[0] == 'Too many changes to the repo'
 
-        oid = artifacts.create_file(str(folder_path), "second_file.txt", "lorem ipsum", False, False)
-       
-        user_envs_tree = get_user_envs_tree(artifacts, oid)
-        assert "second_file.txt" in [obj.name for obj in user_envs_tree[new_test_env]]
+        oid = artifacts.create_file(
+            str(folder_path), "second_file.txt", "lorem ipsum", False, False
+        )
 
-        with pytest.raises(FileExistsError) as exc_info:   
-            artifacts.create_file(str(folder_path), "file.txt", "lorem ipsum", False, False)
+        user_envs_tree = get_user_envs_tree(artifacts, oid)
+        assert "second_file.txt" in [
+            obj.name for obj in user_envs_tree[new_test_env]
+        ]
+
+        with pytest.raises(FileExistsError) as exc_info:
+            artifacts.create_file(
+                str(folder_path), fname, "lorem ipsum", False, False
+            )
         assert exc_info.value.args[0] == 'File already exists'
-        
-        oid = artifacts.create_file(str(folder_path), "file.txt", "override", False, True)
+
+        oid = artifacts.create_file(
+            str(folder_path), fname, "override", False, True
+        )
 
         user_envs_tree = get_user_envs_tree(artifacts, oid)
-        assert "file.txt" in [obj.name for obj in user_envs_tree[new_test_env]]
-        assert user_envs_tree[new_test_env]["file.txt"].data.decode() == "override"
+        assert fname in [obj.name for obj in user_envs_tree[new_test_env]]
+        assert user_envs_tree[new_test_env][fname].data.decode() == "override"
+
 
 def test_delete_environment():
     artifacts = Artifacts()
     with copy_of_repo(artifacts):
         new_test_env = "test_create_file_env"
-        folder_path = Path(get_user_path_without_environments(artifacts), new_test_env)
-        oid = artifacts.create_file(str(folder_path), "file.txt", "lorem ipsum", True, False)
+        folder_path = Path(
+            get_user_path_without_environments(artifacts), new_test_env
+        )
+        oid = artifacts.create_file(
+            str(folder_path), "file.txt", "lorem ipsum", True, False
+        )
         artifacts.commit(artifacts.repo, oid, "commit file")
 
         user_envs_tree = get_user_envs_tree(artifacts, oid)
         assert new_test_env in [obj.name for obj in user_envs_tree]
 
-        oid = artifacts.delete_environment(new_test_env, get_user_path_without_environments(artifacts), oid)
+        oid = artifacts.delete_environment(
+            new_test_env, get_user_path_without_environments(artifacts), oid
+        )
 
         artifacts.commit(artifacts.repo, oid, "commit file")
 
         user_envs_tree = get_user_envs_tree(artifacts, oid)
         assert new_test_env not in [obj.name for obj in user_envs_tree]
 
-
-
-# refactor test stuff above that is repeated a lot
 
 # consider if build_tree is needed by replacing with something simpler
