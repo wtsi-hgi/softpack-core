2663bc7bbb608451ae31c6aa850f930e64ebb514
Samuel Ogg <56794292+SamO135@users.noreply.github.com>
2023-08-16 17:13:03 +0100

    add tests for update

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -12,17 +12,19 @@ from softpack_core.schemas.environment import (
     PackageInput,
     InvalidInputError,
     EnvironmentAlreadyExistsError,
+    UpdateEnvironmentSuccess,
+    EnvironmentNotFoundError,
 )
 
 from .test_pygit import copy_of_repo, get_user_path_without_environments
 
 
-def test_create(mocker):
+def test_create_and_update(mocker):
     artifacts = Artifacts()
     environment = Environment(
         id="", name="", path="", description="", packages=[], state=None
     )
-    with copy_of_repo(artifacts) as temp_dir:
+    with copy_of_repo(artifacts):
         environment.artifacts = artifacts
         push_mock = mocker.patch('pygit2.Remote.push')
         post_mock = mocker.patch('httpx.post')
@@ -33,8 +35,9 @@ def test_create(mocker):
             description="description",
             packages=[PackageInput(name="pkg_test")],
         )
-        result = environment.create(env_input)
 
+        # Test create
+        result = environment.create(env_input)
         assert isinstance(result, CreateEnvironmentSuccess)
         push_mock.assert_called_once()
         post_mock.assert_called_once()
@@ -58,4 +61,22 @@ def test_create(mocker):
         env_input.path = "invalid/path"
         result = environment.create(env_input)
         assert isinstance(result, InvalidInputError)
+
+        # Test update
+        env_input.path = str(get_user_path_without_environments(artifacts))
+        env_input.description = "updated description"
+        result = environment.update(env_input, env_input.path, env_input.name)
+        assert isinstance(result, UpdateEnvironmentSuccess)
+
+        result = environment.update(env_input, "invalid/path", "invalid_name")
+        assert isinstance(result, InvalidInputError)
+
+        env_input.name = ""
+        result = environment.update(env_input, "invalid/path", "invalid_name")
+        assert isinstance(result, InvalidInputError)
+
+        env_input.name = "invalid_name"
+        env_input.path = "invalid/path"
+        result = environment.update(env_input, "invalid/path", "invalid_name")
+        assert isinstance(result, EnvironmentNotFoundError)
 
